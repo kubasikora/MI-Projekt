7115c8caa050ff3a102371af0be037dc3637a260
rgryta <radek.gryta@gmail.com>
2020-05-31 19:15:32 +0200

    Wyniki i prezentacja

--- matlab/wykresy/generate_graph.m
+++ matlab/wykresy/generate_graph.m
@@ -8,7 +8,7 @@ close all
 variables = T.Properties.VariableNames;
 outputNames = {'DP','LT01'};
 
-selected_out=1;
+selected_out=2;
 if selected_out==1
     load('out1v1.mat')
 else
@@ -35,16 +35,22 @@ n = [na nb nk];
 [sys]=prep_system(T,outputNames,sel_var,Ts, x);
 
 %testing result
-figure;
-
 result = 0;
 for i=1:4
     [y,testy,~,~,own_fit,testt]=test_data(i,outputNames,sys,sel_var,t);
-    subplot(2,2,i);
+    fig = figure;
     hold on;
-    plot(testt,y.y);
-    plot(testt,testy);
-    legend('Identified model','True output');
+    plot(testt,testy,'r-','LineWidth',1.5);
+    plot(testt,y.y,'b-','LineWidth',1.5);
+    legend('y_{real}','y_{model}');
+    ylabel(outputNames{selected_out});
+    xlabel('Czas[s]');
+    grid on;
     result = result+abs(own_fit);
+    set(fig,'Position',[1000 300 1000 600]);
+    
+    filename=strcat(outputNames{selected_out},'_',num2str(i));
+    saveas(fig,filename,'epsc')
+    close all;
 end
 
